cc2125bcc1a7446b1d30840e501f5071cdc3591f
Keilly Leyva <54252495+leyvaKeilly@users.noreply.github.com>
2020-05-02 22:18:14 -0400

    Update memIO.sv

--- final_project/final_project.srcs/sources_1/new/memIO.sv
+++ final_project/final_project.srcs/sources_1/new/memIO.sv
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/08/2020 04:29:06 PM
-// Design Name: 
-// Module Name: memIO
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module memIO #(
